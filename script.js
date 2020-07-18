3fe119166eee744ead81e684c7d7286de73774e2
mahnoor1999 <57394804+mahnoor1999@users.noreply.github.com>
2020-07-18 23:22:24 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,86 +1,106 @@
 
-// apple
-//  Initial value of apple
+
+
 let appleprice = 0;
-let appleQuantity = 0
+let appleQuantity = 0;
+let mangoprice = 0;
+let mangoQuantity = 0;
+let bananaprice = 0;
+let bananaQuantity = 0;
+let total = 0;
+let budget = 0;
+let remaining = 0;
 
 
+function user_budget() {
+    budget = document.querySelector("#budget").value;
+}
+function total_amount() {
+    total = appleprice + mangoprice + bananaprice;
+    document.querySelector("#total span").innerHTML = total;
+}
+function user_remaining() {
+    remaining = budget - total;
+    document.querySelector("#r-b span").innerHTML = remaining;
+}
+
+// apple
 // adding apple price
-
 function addprice1() {
+    user_budget();
+    if (total != budget && total<=budget) {
         appleprice += 10
+        total_amount();
         appleQuantity++
         document.querySelector("#apple-price").value = appleQuantity;
-        let total = appleprice + mangoprice + bananaprice
-        document.querySelector("#total span").innerHTML = total;
-        let budget = document.querySelector("#budget").value;
-        let remaining = budget - total;
-        document.querySelector("#r-b span").innerHTML = remaining;
+        user_remaining();
+    }
+    else {
+        alert("You don't have an enough amount")
+    }
 }
 // subtracting apple price
 function subprice1() {
     appleprice -= 10
     appleQuantity--
     document.querySelector("#apple-price").value = appleQuantity;
-    let total = appleprice + mangoprice + bananaprice
-    document.querySelector("#total span").innerHTML = total;
-    let budget = document.querySelector("#budget").value;
-    let remaining = budget - total;
-    document.querySelector("#r-b span").innerHTML = remaining;
+
+    user_budget();
+    total_amount();
+    user_remaining();
 }
 // mango
-//  Initial value of mango
-let mangoprice = 0;
-let mangoQuantity = 0
-
 // adding mango price
 function addprice2() {
-    mangoprice += 5
-    mangoQuantity++
-    document.querySelector("#mango-price").value = mangoQuantity;
-    let total = appleprice + mangoprice + bananaprice
-    document.querySelector("#total span").innerHTML = total;
-    let budget = document.querySelector("#budget").value;
-    let remaining = budget - total;
-    document.querySelector("#r-b span").innerHTML = remaining;
+    user_budget();
+    if (total != budget  && total<=budget) {
+        mangoprice += 5
+        total_amount();
+        mangoQuantity++
+        document.querySelector("#mango-price").value = mangoQuantity;
+        user_remaining();
+    }
+    else {
+        alert("You don't have an enough amount")
+    }
+
+
 }
 // subtracting mango price
 function subprice2() {
     mangoprice -= 5
     mangoQuantity--
     document.querySelector("#mango-price").value = mangoQuantity;
-    let total = appleprice + mangoprice + bananaprice
-    document.querySelector("#total span").innerHTML = total;
-    let budget = document.querySelector("#budget").value;
-    let remaining = budget - total;
-    document.querySelector("#r-b span").innerHTML = remaining;
+
+    user_budget();
+    total_amount();
+    user_remaining();
 }
 // banana
-//  Initial value of banana
-let bananaprice = 0;
-let bananaQuantity = 0
-
 // adding banana price
 function addprice3() {
-    bananaprice += 15
-    bananaQuantity++
-    document.querySelector("#banana-price").value = bananaQuantity;
-    let total = appleprice + mangoprice + bananaprice
-    document.querySelector("#total span").innerHTML = total;
-    let budget = document.querySelector("#budget").value;
-    let remaining = budget - total;
-    document.querySelector("#r-b span").innerHTML = remaining;
+    user_budget();
+    if (total != budget  && total<=budget) {
+        bananaprice += 15
+        total_amount();
+        bananaQuantity++
+        document.querySelector("#banana-price").value = bananaQuantity;
+        user_remaining();
+    }
+    else {
+        alert("You don't have an enough amount")
+    }
 }
 // subtracting banana price
 function subprice3() {
     bananaprice -= 15
     bananaQuantity--
     document.querySelector("#banana-price").value = bananaQuantity;
-    let total = appleprice + mangoprice + bananaprice
-    document.querySelector("#total span").innerHTML = total;
-    let budget = document.querySelector("#budget").value;
-    let remaining = budget - total;
-    document.querySelector("#r-b span").innerHTML = remaining;
+
+    user_budget();
+    total_amount();
+    user_remaining();
+
 }
 
 
